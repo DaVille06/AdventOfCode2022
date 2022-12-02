11b45f7451814b88786252e9faf246c5cb54131d
Nick Roberts <roberts.nicholas88@gmail.com>
2022-12-01 20:58:17 -0500

    Solution to puzzle 1.

---
+++ day1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace day1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var calorieInputs = File.ReadAllLines("input.txt");
+        var currentSum = 0;
+        var maxSum = 0;
+
+        foreach (var snack in calorieInputs)
+        {
+            if (string.IsNullOrEmpty(snack))
+            {
+                maxSum = Math.Max(currentSum, maxSum);
+                currentSum = 0;
+            }
+            else
+            {
+                currentSum += int.Parse(snack);
+            }
+        }
+
+        Console.WriteLine(Math.Max(currentSum, maxSum));
+    }
+}

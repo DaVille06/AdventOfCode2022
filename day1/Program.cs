8b637cc4df549d7bcfcbdb676b481557a621a5ad
Nick Roberts <roberts.nicholas88@gmail.com>
2022-12-01 21:44:15 -0500

    Completed part 2 of Day1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,8 +1,12 @@
-﻿namespace day1;
+﻿using System.Linq;
+
+namespace day1;
 class Program
 {
     static void Main(string[] args)
     {
+
+        // PUZZLE 1
         var calorieInputs = File.ReadAllLines("input.txt");
         var currentSum = 0;
         var maxSum = 0;
@@ -21,5 +25,32 @@ class Program
         }
 
         Console.WriteLine(Math.Max(currentSum, maxSum));
+
+        // PUZZLE 2
+        // resetting current since we used it above
+        currentSum = 0;
+        var top3 = new List<int>();
+        foreach (var snack in calorieInputs)
+        {
+            if (string.IsNullOrEmpty(snack))
+            {
+                top3.Add(currentSum);
+
+                if (top3.Count > 3)
+                {
+                    // we need to remove lowest
+                    top3.Remove(top3.Min());
+                }
+
+                // reset current sum
+                currentSum = 0;
+            }
+            else
+            {
+                currentSum += int.Parse(snack);
+            }
+        }
+
+        Console.WriteLine(top3.Sum());
     }
 }
